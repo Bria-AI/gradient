4112d6276c9e65568e26c680a818e8e308f94e07
galbria <gal@bria.ai>
2025-02-03 11:16:28 +0000

    arg is a dict

--- gradient/core/config/bria4B_adapt.py
+++ gradient/core/config/bria4B_adapt.py
@@ -263,24 +263,24 @@ class HyperParemeter(BaseModel):
     def get_hyperparameter(self) -> dict:
         args = self.model_dump()
 
-        print(f"flow_matching_latent_loss: {args["flow_matching_latent_loss"]}")
+        # print(f"flow_matching_latent_loss: {args["flow_matching_latent_loss"]}")
 
         env_local_rank = int(os.environ.get("LOCAL_RANK", -1))
-        if env_local_rank != -1 and env_local_rank != args.local_rank:
-            args.local_rank = env_local_rank
+        if env_local_rank != -1 and env_local_rank != args["local_rank"]:
+            args["local_rank"] = env_local_rank
 
         # Sanity checks
-        if args.dataset_name is None and args.data_channels == "":
+        if args.get("dataset_name") is None and args.get("data_channels", "") == "":
             raise ValueError("Need either a dataset name or a training folder.")
 
         assert (
-            args.s3_prefix is not None
+            args["s3_prefix"] is not None
         ), "s3_prefix must be specified, i.e., dir for saving checkpoints at s3"
 
         # Init boolean args that are ints
-        args.reinit_scheduler = args.reinit_scheduler == 1
-        args.use_adafactor = args.use_adafactor == 1
-        args.reinit_optimizer = args.reinit_optimizer == 1
-        args.train_with_ratios == (args.train_with_ratios == 1)
+        args["reinit_scheduler"] = args["reinit_scheduler"] == 1
+        args["use_adafactor"] = args["use_adafactor"] == 1
+        args["reinit_optimizer"] = args["reinit_optimizer"] == 1
+        args["train_with_ratios"] = args["train_with_ratios"] == 1
 
         return args
